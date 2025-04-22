2e46aaa5d27c4a1a7585e8b6c20337a845a4b46b
FrankieFong-hk <httfong@gmail.com>
2025-04-22 17:02:23 -0400

    Initialize MERN stack application with basic product management features, including server setup, database connection, and CRUD operations for products. Added .gitignore, package.json, and necessary backend files.

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+// const express = require("express");
+
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json()); // allow us to accept JSON data in the req.body
+
+app.use("/api/products", productRoutes);
+
+
+app.listen(3000, () => {
+    connectDB();
+    console.log("Server is running on port 3000");
+});
+
+// fwb51KRXiuGR2yvu
